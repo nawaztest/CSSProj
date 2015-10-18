d01766601996d01c4223f249e750f54b8c809778
unknown <Nawazikram@Nawaz-PC.vpn.servicetitan.com>
2015-10-17 20:33:35 -0700

    Initial Commit

---
+++ CSSProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSSProj.Startup))]
+namespace CSSProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
